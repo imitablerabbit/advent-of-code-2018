60ab4957639dce59897609d284e37f9817f0f05a
Mark Hillman <markhillman94@googlemail.com>
2018-12-10 22:04:58 +0000

    Start of day 6

---
+++ day6/coords.go
@@ -0,0 +1,9 @@
+// Find largest x and y coords so we can get the size of the array
+
+// Create array and fill it up with the calculated manhatten distance for all of the coordinates
+
+// Loop through and find the coordinate with the lowest value
+
+// Loop through and calculate the areas for all of the coords
+
+// If we reach the bounds of the array, then it is an infinitely sprawling one.
